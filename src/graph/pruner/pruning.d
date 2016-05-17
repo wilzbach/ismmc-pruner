5d3e3ef95e750670baaf57027270a85669751b01
Sebastian Wilzbach <seb@wilzba.ch>
2016-05-17 14:27:16 +0300

    pruning with maxFlow

---
+++ src/graph/pruner/pruning.d
@@ -0,0 +1,11 @@
+module pruner.pruning;
+
+import pruner.app;
+import pruner.formats;
+
+auto maxFlowPruning(immutable(Read)[] reads, edge_t maxReadsPerPos)
+{
+    //auto opt = maxFlowOpt(reads, maxReadsPerPos);
+    //const(Read)*[] pruned = opt.flow.prune;
+    return [0];
+}
