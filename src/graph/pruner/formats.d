e581337b753ccf3ae3f595fc461f8dab804af6be
Sebastian Wilzbach <seb@wilzba.ch>
2016-05-18 12:19:41 +0300

    add a simple contains function

--- src/graph/pruner/formats.d
+++ src/graph/pruner/formats.d
@@ -13,10 +13,13 @@ struct Read
     size_t id;
 
     // prevent accidental copies
+    // TODO: this makes it a non-copyable type, however it is a pain to work with
+    // because most algorithms do automatic copies
     @disable this(this);
 }
 
-bool equals()(auto ref const(Read)*[] a, auto ref Read[] b)
+// TODO: if we would be able to do copies, this should be easier
+bool equals(const(Read)*[] a, Read[] b)
 {
     import std.range: empty, front, popFront;
     while (!a.empty)
@@ -28,4 +31,32 @@ bool equals()(auto ref const(Read)*[] a, auto ref Read[] b)
     }
     if (!b.empty) return false;
     return true;
+}
+
+unittest
+{
+    auto reads = [Read(0, 8), Read(0, 2), Read(1, 10)];
+
+    const(Read)*[] constReads = [&reads[0], &reads[2]];
+    assert(!constReads.equals(reads));
+
+    const(Read)*[] constReadsEqual = [&reads[0], &reads[1], &reads[2]];
+    assert(constReadsEqual.equals(reads));
+}
+
+bool contains(const(Read)*[] reads, ref Read r)
+{
+    foreach (read; reads)
+        if ((*read).start == r.start && (*read).end == r.end)
+            return true;
+
+    return false;
+}
+
+unittest
+{
+    auto reads = [Read(0, 8), Read(0, 2), Read(1, 3), Read(1, 10)];
+    const(Read)*[] constReads = [&reads[0], &reads[2]];
+    assert(constReads.contains(reads[0]));
+    assert(!constReads.contains(reads[1]));
 }
--- src/graph/pruner/pruning.d
+++ src/graph/pruner/pruning.d
@@ -2,23 +2,11 @@ module pruner.pruning;
 
 import pruner.app;
 import pruner.formats;
+import std.experimental.logger;
 
 const(Read)*[] maxFlowPruning(R)(R reads, edge_t maxReadsPerPos)
 {
     //const(Read)*[] prunes;
-
-    //import pruner.functions;
-    //auto accumulatedCov = reads.accumulateCov;
-    //while (!accumulatedCov.empty)
-
     auto opt = maxFlowOpt(reads, maxReadsPerPos);
     return prune(opt.flow);
 }
-
-//unittest
-//{
-    //import std.algorithm: maxPos, minPos;
-    //auto reads = [Read(0, 8), Read(10, 12)];
-    //assert(reads.accumulateCov.maxPos.front == 1);
-    //assert(reads.accumulateCov.minPos.front == 0);
-//}
