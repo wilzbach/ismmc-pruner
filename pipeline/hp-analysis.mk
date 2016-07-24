83e0920487f381a25a0a3682ecc9353c26131264
Sebastian Wilzbach <seb@wilzba.ch>
2016-07-24 18:24:47 +0200

    use default coverage for whatshap

--- pipeline/hp-analysis.mk
+++ pipeline/hp-analysis.mk
@@ -3,7 +3,7 @@
 ################################################################################
 
 %.hp.normal.vcf %.hp.normal.log: %.gvcf %.samsorted.bam | $(WHATSHAP)
-	$(WHATSHAP) phase $^ -o $*.hp.normal.vcf 2> $*.hp.normal.log
+	$(WHATSHAP) phase --max-coverage $(READ_COVERAGE) $^ -o $*.hp.normal.vcf 2> $*.hp.normal.log
 
 %.hp.pruned.vcf %.hp.pruned.log: %.gvcf %.pruned.filtered.bam %.pruned.filtered.bam.bai | $(WHATSHAP)
 	$(WHATSHAP) phase $< $(word 2,$^) -o $*.hp.pruned.vcf 2>  $*.hp.pruned.log
