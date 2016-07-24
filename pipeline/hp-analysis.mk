657d50cf11bc3145de90e00ec6dc75ed419c375a
Sebastian Wilzbach <seb@wilzba.ch>
2016-07-25 01:57:50 +0200

    provide reference to whatshap

--- pipeline/hp-analysis.mk
+++ pipeline/hp-analysis.mk
@@ -1,15 +1,20 @@
 ################################################################################
 # WhatsHap Analysis
 ################################################################################
+%.hp.normal.vcf %.hp.normal.log: %.gvcf %.samsorted.bam $$(@D)/ref.fa $$(@D)/ref.fa.fai | $(WHATSHAP)
+	$(WHATSHAP) phase --reference $(word 3, $^) --max-coverage $(READ_COVERAGE) $< $(word 2,$^) -o $*.hp.normal.vcf 2> $*.hp.normal.log
 
-%.hp.normal.vcf %.hp.normal.log: %.gvcf %.samsorted.bam | $(WHATSHAP)
-	$(WHATSHAP) phase --max-coverage $(READ_COVERAGE) $^ -o $*.hp.normal.vcf 2> $*.hp.normal.log
-
-%.hp.pruned.vcf %.hp.pruned.log: %.gvcf %.pruned.filtered.bam %.pruned.filtered.bam.bai | $(WHATSHAP)
-	$(WHATSHAP) phase $< $(word 2,$^) -o $*.hp.pruned.vcf 2>  $*.hp.pruned.log
+%.hp.pruned.vcf %.hp.pruned.log: %.gvcf %.pruned.filtered.bam %.pruned.filtered.bam.bai $$(@D)/ref.fa $$(@D)/ref.fa.fai | $(WHATSHAP)
+	$(WHATSHAP) phase --reference $(word 4, $^) --max-coverage $(READ_COVERAGE) $< $(word 2,$^) -o $*.hp.pruned.vcf 2>  $*.hp.pruned.log
 
 %.hp.compare: %.hp.normal.log %.hp.pruned.log
 	@echo "===normal===="
 	@grep "No. of" $<
 	@echo "===pruned===="
 	@grep "No. of" $(word 2, $^)
+
+################################################################################
+# Pruned WhatsHap Analysis
+################################################################################
+
+%.gt.compare: %.pgvcf %.gvcf
